d46bae360aa13a2ebe0f610fa6866e3932d78722
HashToBeWild <loki@swarm.technology>
2021-07-05 20:33:36 +0200

    Initial

---
+++ Innumerati/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Innumerati
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
